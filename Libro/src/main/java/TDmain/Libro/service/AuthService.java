b54be92f20f1a62e201e350fc7595577ca5ba721
RenzoTorresA <133610404+RenzoTorresA@users.noreply.github.com>
2023-11-11 18:48:54 -0500

    Create AuthService.java
    Crear la clase servicios que implementara el repositorio

---
+++ Libro/src/main/java/TDmain/Libro/service/AuthService.java
@@ -0,0 +1,49 @@
+package TDmain.Libro.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import TDmain.Libro.model.modelAutor;
+import TDmain.Libro.repository.IAuhtRepository;
+
+@Service
+public class AuthService {
+    @Autowired
+    IAuhtRepository auhtRepository;
+
+    public List<modelAutor> getAllAutor(){
+        return(List<modelAutor>) auhtRepository.findAll();
+    }
+
+    public modelAutor saveAutor(modelAutor autor){
+        return auhtRepository.save(autor);
+    }
+
+    public Optional<modelAutor> getById(Integer id){
+        return auhtRepository.findById(id);
+    }
+
+    public modelAutor updateById(modelAutor request, Integer id){
+        modelAutor autor = auhtRepository.findById(id).get();
+
+        autor.setNombre(request.getNombre());
+        autor.setApellido(request.getApellido());
+        autor.setDni(request.getDni());
+        autor.setEmails(request.getEmails());
+
+        auhtRepository.save(autor);
+        return autor;
+    }
+
+    public Boolean deleteAutor(Integer id){
+        try{
+            auhtRepository.deleteById(id);
+            return true;
+        }catch(Exception e){
+            return false;
+        }
+    }
+}
