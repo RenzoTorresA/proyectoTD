624085c89d7de1d0481bfa24fb24fd27a0964a4e
RenzoTorresA <133610404+RenzoTorresA@users.noreply.github.com>
2023-12-19 10:54:35 -0500

    request, response  y log
    Implementar un Log de Errores
    Implementar los Request y Response usando un mapper

--- Libro/src/main/java/TDmain/Libro/Controller/AuthController.java
+++ Libro/src/main/java/TDmain/Libro/Controller/AuthController.java
@@ -4,29 +4,38 @@ import java.util.List;
 
 import java.util.stream.Collectors;
 
-
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import TDmain.Libro.ControllerEx.ApiResponse;
 import TDmain.Libro.dto.AuthRequest;
 
 import TDmain.Libro.model.AutorMappers;
+import TDmain.Libro.model.UsuarioNotFoundException;
 import TDmain.Libro.model.modelAutor;
 
 import TDmain.Libro.service.IAuthService;
+
 
 
 @RestController
 @RequestMapping("/api/autor")
 public class AuthController {
     
+    private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
+
     @Autowired
     IAuthService authService;
 
@@ -34,36 +43,92 @@ public class AuthController {
     AutorMappers autorMappers;
 
     @GetMapping("/getAll")
-    public List<AuthRequest> getAll(){
+    public ResponseEntity<ApiResponse<List<AuthRequest>>> getAll(){
 
+           try{
             List<modelAutor> autores = authService.findAll();
-            return autores.stream().map(autorMappers::entityToDto).collect(Collectors.toList());
+            List<AuthRequest> authRequests = autores.stream()
+            .map(autorMappers::entityToDto).collect(Collectors.toList());
+            return ResponseEntity.ok(new ApiResponse<>(HttpStatus.OK.value(),
+             "consulta exitosa", authRequests));
+        }catch(Exception ex){
+            logger.error("Error en el metodo getAll", ex);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body(new ApiResponse<>(HttpStatus.INTERNAL_SERVER_ERROR.value(), 
+            "Error al procesar la solicitud", null));
+           } 
         
     }
 
     @PostMapping("/create")
-    public AuthRequest create(@RequestBody AuthRequest authRequest){
-        modelAutor autor = autorMappers.dtoToEntity(authRequest);
-        autor = authService.add(autor);
-        return autorMappers.entityToDto(autor);
+    public ResponseEntity<ApiResponse<AuthRequest>> create(@RequestBody AuthRequest authRequest){
+        try{
+            modelAutor autores = autorMappers.dtoToEntity(authRequest);
+            autores = authService.add(autores);
+            AuthRequest creaAuthRequest = autorMappers.entityToDto(autores);
+            return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse<>(HttpStatus.CREATED.value(), "Usuario Creado", creaAuthRequest));
+        }catch(Exception ex){
+            logger.error("Error en el metodo CREATE", ex);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse<>(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Error al Crear", null));
+        }
         
     }
 
     @GetMapping("/getById")
-    public AuthRequest getById(int id){
-        modelAutor autor = authService.findById(id);
-        return autorMappers.entityToDto(autor);
+    public ResponseEntity<ApiResponse<AuthRequest>> getById(@RequestParam int id){
+        try{
+            modelAutor autores = authService.findById(id);
+            if (autores !=null) {
+                AuthRequest authRequest = autorMappers.entityToDto(autores);
+                return ResponseEntity.ok(new ApiResponse<>(HttpStatus.OK.value(), "Consulta de Busqueda por ID extitosa", authRequest));
+            }else{
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse<>(HttpStatus.NOT_FOUND.value(), "Usuario no creado",null ));
+            }
+        }catch(Exception ex){
+            logger.error("Error en el metodo getById", ex);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse<>(HttpStatus.INTERNAL_SERVER_ERROR.value(), 
+            "Error al Buscar Autor" , null));
+        }
     }
 
     @PutMapping("/update")
-    public AuthRequest update(@RequestBody AuthRequest authRequest){
-        modelAutor autor = autorMappers.dtoToEntity(authRequest);
-        autor = authService.update(autor);
-        return autorMappers.entityToDto(autor);
+    public ResponseEntity<ApiResponse<AuthRequest>> update(@RequestBody AuthRequest authRequest){
+        try{
+            // Llamada al servicio para actualizar el usuario
+            modelAutor autores = authService.update(autorMappers.dtoToEntity(authRequest));
+            // Construir la respuesta exitosa
+            AuthRequest actualizarAuthRequest = autorMappers.entityToDto(autores);
+            return ResponseEntity.ok(new ApiResponse<>(HttpStatus.OK.value(), "Usuario actualizado", actualizarAuthRequest));
+        }catch(UsuarioNotFoundException ex){
+            logger.error("Error al actualizar Autor", ex);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse<>(HttpStatus.NOT_FOUND.value(), ex.getMessage(), null));
+        }
+        catch(Exception ex){
+            logger.error("Error en el metodo UPDATE", ex);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse<>(HttpStatus.INTERNAL_SERVER_ERROR.value(),
+            "Error al actulizar Autor", null));
+        }
     }
 
     @DeleteMapping("/delete")
-    public boolean deleteById(int id){
-        return authService.delete(id);
+    public ResponseEntity<ApiResponse<String>> delete(@PathVariable int id){
+        try{
+            // Llamada al servicio para eliminar el usuario
+            boolean elimminar = authService.delete(id);
+
+            if (elimminar) {
+                return ResponseEntity.ok(new ApiResponse<>(HttpStatus.OK.value(), "Usuario Eliminado correctamente", null));
+            }else{
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse<>(HttpStatus.NOT_FOUND.value(), 
+                "No se Encontro Autor a eliminar", null));
+            }
+        }catch(Exception ex){
+            // Registrar el error utilizando SLF4J
+            logger.error("Error en el metodo DELETE", ex);
+
+            // Manejar otras excepciones y devolver una respuesta 500 Internal Server Error con un mensaje genérico
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse<>(HttpStatus.INTERNAL_SERVER_ERROR.value(), 
+            "Error al aliminar usuario", null));
+        }
     }
 }
