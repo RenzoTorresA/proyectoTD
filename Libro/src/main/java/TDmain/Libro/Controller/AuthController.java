100c9ec4bb6e9a5e0e7462df7e64a9c2cf0f2213
RenzoTorresA <133610404+RenzoTorresA@users.noreply.github.com>
2023-12-19 00:53:45 -0500

    Implementar los Request y Response usando un mapper

--- Libro/src/main/java/TDmain/Libro/Controller/AuthController.java
+++ Libro/src/main/java/TDmain/Libro/Controller/AuthController.java
@@ -1,56 +1,67 @@
 package TDmain.Libro.Controller;
 
 import java.util.List;
-import java.util.Optional;
+
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import TDmain.Libro.dto.AuthRequest;
+import TDmain.Libro.model.AutorMappers;
 import TDmain.Libro.model.modelAutor;
-import TDmain.Libro.service.AuthService;
+
+import TDmain.Libro.service.IAuthService;
+
 
 @RestController
 @RequestMapping("/api/autor")
 public class AuthController {
     
     @Autowired
-    AuthService authService;
+    IAuthService authService;
 
-    @GetMapping
-    public List<modelAutor> get(){
-        return authService.getAllAutor();
+    @Autowired
+    AutorMappers autorMappers;
+
+    @GetMapping("/getAll")
+    public List<AuthRequest> getAll(){
+
+            List<modelAutor> autores = authService.findAll();
+            return autores.stream().map(autorMappers::entityToDto).collect(Collectors.toList());
+        
     }
 
-    @PostMapping
-    public modelAutor saveAutor(@RequestBody modelAutor autor){
-        return this.authService.saveAutor(autor);
+    @PostMapping("/create")
+    public AuthRequest create(@RequestBody AuthRequest authRequest){
+        modelAutor autor = autorMappers.dtoToEntity(authRequest);
+        autor = authService.add(autor);
+        return autorMappers.entityToDto(autor);
+        
     }
 
-    @GetMapping(path = "/{id}")
-    public Optional<modelAutor> getAutorById(@PathVariable("id") Integer id){
-        return this.authService.getById(id);
+    @GetMapping("/getById")
+    public AuthRequest getById(int id){
+        modelAutor autor = authService.findById(id);
+        return autorMappers.entityToDto(autor);
     }
 
-    @PutMapping(path = "/{id}")
-    public modelAutor updateAutorById(@RequestBody modelAutor request, @PathVariable("id") Integer id){
-        return this.authService.updateById(request, id);
+    @PutMapping("/update")
+    public AuthRequest update(@RequestBody AuthRequest authRequest){
+        modelAutor autor = autorMappers.dtoToEntity(authRequest);
+        autor = authService.update(autor);
+        return autorMappers.entityToDto(autor);
     }
 
-    @DeleteMapping(path = "/{id}")
-    public String deleteById(@PathVariable("id") Integer id){
-        boolean eliminar = this.authService.deleteAutor(id);
-
-        if (eliminar) {
-            return "Usuario con ID "+ id + " eliminado";
-        }else{
-            return "error al eliminar";
-        }
+    @DeleteMapping("/delete")
+    public boolean deleteById(int id){
+        return authService.delete(id);
     }
 }
